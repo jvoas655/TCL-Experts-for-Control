568ec1f9d8f2c0841d1b3f0c8e6238cb797052d3
Alex Chandler <alex3@Alexs-MacBook-Pro-4.local>
2022-11-13 16:19:51 -0600

    initial gpt finetuning implementation, using dataset.py and GPT_With_Adapter_Modules

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
-from transformers import RobertaTokenizer
+from transformers import RobertaTokenizer, GPT2Tokenizer
 from tqdm import tqdm
 import h5py
 
@@ -75,6 +75,60 @@ class WDMCEncDataset(Dataset):
             "input_ids": self.tokens["input_ids"][idx, :],
             "attention_mask": self.tokens["attention_mask"][idx, :],
         }
+        sample = {
+            "encodings": self.encodings[idx, :],
+            "tokens": tokens
+        }
+        return sample
+
+class WDMCGPTEncDataset(Dataset):
+
+    def __init__(self, file_path, split, target_enc_key, model_name, token_count, batch_size):
+        assert split in ["test", "train", "val"]
+        self.encodings = []
+        with h5py.File(file_path, "r") as file_ref:
+            split_data = file_ref[split][target_enc_key]
+            for key in tqdm(split_data.keys(), total = len(split_data.keys()), desc=f"Loading {split} encodings"):
+                self.encodings.append(split_data[key][()].reshape(1, -1))
+        self.encodings = np.concatenate(self.encodings)
+
+        self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+        self.tokenizer.padding_side = "left"
+        self.tokenizer.pad_token = self.tokenizer.eos_token # TODO: verify this is the desired padding
+        self.sentences = []
+        with h5py.File(file_path, "r") as file_ref:
+            split_data = file_ref[split]["text"]
+            for key in tqdm(split_data.keys(), total = len(split_data.keys()), desc=f"Loading {split} text"):
+                summary = split_data[key][()].decode("utf-8")
+                first_sentence = summary.split(".")[0] + "."
+                self.sentences.append(first_sentence)
+        
+        self.tokens = []
+        for i in tqdm(range(0, len(self.sentences), batch_size), desc = f"Tokenizing {split} encodings", total = len(self.sentences) // batch_size):
+            self.tokens.append(self.tokenizer(self.sentences[i:i+batch_size], return_tensors="pt", truncation=True, padding=True))
+        # TODOL issue, each batch has a different size of 64,x
+        input_ids = torch.cat([ind["input_ids"] for ind in self.tokens])
+        attention_mask = torch.cat([ind["attention_mask"] for ind in self.tokens])
+        self.tokens = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+        }
+        
+    def __len__(self):
+        return len(self.encodings)
+    
+    def output_size(self):
+        return self.encodings.shape[1]
+
+    # TODO: is __getitem__ for GPT as roberta
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        tokens = {
+            "input_ids": self.tokens["input_ids"][idx, :],
+            "attention_mask": self.tokens["attention_mask"][idx, :],
+        }
+        # TODO: why sample?
         sample = {
             "encodings": self.encodings[idx, :],
             "tokens": tokens
