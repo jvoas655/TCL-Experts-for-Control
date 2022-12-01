a0fab9bad37ed7c94eb59493e4310c5670915d4e
Alex Chandler <alex3@wireless-10-145-23-238.public.utexas.edu>
2022-12-01 15:56:14 -0600

    got working BLEU scores I believe

--- src/eval/MetricEvaluator.py
+++ src/eval/MetricEvaluator.py
@@ -6,6 +6,8 @@ from dataclasses import dataclass
 from bleu import list_bleu, multi_list_bleu
 from rouge_score import rouge_scorer
 from collections import defaultdict
+from nltk.translate.bleu_score import sentence_bleu
+
 
 @dataclass
 class MetricEvaluator:
@@ -25,44 +27,63 @@ class MetricEvaluator:
             else:
                 return self.rouge_scorer.score(reference, hypothesis)
         elif method_type.lower() == "blue":
-            # TODO: This code below is wrong. There are several BLUE python packages, including from nltk.translate.bleu_score import sentence_bleu
+            # Note There are several BLUE python packages, including from nltk.translate.bleu_score import sentence_bleu
             # and a huggingface library equivalent
-            # use if only one hypothesis
-            if len(hypothesis) == 1:
-                assert len(reference) == 1
-                blue_scores = list_bleu([reference], hypothesis)
-                return blue_scores
-            # use multi_list_bleu if multiple hypotheses
-            elif type(hypothesis)== list:
-                # Case 1: only have one hypothesis
-                # if len(reference) == reference == 1:
-                blue_scores = multi_list_bleu([reference], [hypothesis])
-                return blue_scores
-            else:
-                raise NotImplementedError()
+            if type(reference) == list:
+                ref_list = reference
+            elif type(reference) == str:
+                ref_list = [reference]
+            if type(hypothesis) == list:
+                hypo_list = hypothesis
+            elif type(hypothesis) == str:
+                hypo_list = [hypothesis]
+            bleu_scores = defaultdict(list)
+            for ref, hyp in zip(ref_list, hypo_list):
+                ref_split = [ref.split()]
+                hyp_split = hyp.split()
+                
+                #  Calculate BLEU 1-Gram
+                one_gram =  sentence_bleu(ref_split, hyp_split, weights=(1, 0, 0, 0))
+                print('Cumulative 1-gram: %f' % one_gram)
+                bleu_scores["one-gram"].append(one_gram)
+
+                #  Calculate BLEU 2-Gram
+                two_gram =  sentence_bleu(ref_split, hyp_split, weights=(0.5, 0.5, 0, 0))
+                print('Cumulative 2-gram: %f' % two_gram )
+                bleu_scores["two-gram"].append(two_gram)
+
+                #  Calculate BLEU 3-Gram
+                three_gram =  sentence_bleu(ref_split, hyp_split, weights=(0.333, 0.333, .333, 0))
+                print('Cumulative 3-gram: %f' % three_gram )
+                bleu_scores["three_gram"].append(three_gram)
+
+                #  Calculate BLEU 4-Gram
+                four_gram =  sentence_bleu(ref_split, hyp_split, weights=(0.25, 0.25, .25, .25))
+                print('Cumulative 4-gram: %f' % four_gram )
+                bleu_scores["four-gram"].append(four_gram)
+                
+            return bleu_scores
+            # if len(hypothesis) == 1:
+            #     assert len(reference) == 1
+            #     blue_scores = list_bleu([reference], hypothesis)
+            #     return blue_scores
+            # # use multi_list_bleu if multiple hypotheses
+            # elif type(hypothesis)== list:
+            #     # Case 1: only have one hypothesis
+            #     # if len(reference) == reference == 1:
+            #     blue_scores = multi_list_bleu([reference], [hypothesis])
+            #     return blue_scores
+            # else:
+            #     raise NotImplementedError()
             # blue_score = list_bleu([[reference[0]]], [hypothesis[0]])
             # return blue_score
         else:
             raise NotImplementedError()
 
 evaluator = MetricEvaluator()
-# ref = ['The quick brown fox jumps over the lazy dog', "Reference sentence"]
-# hypothesis = [ 'The quick brown dog jumps on the log.', "Totally unrelated hypothesis"]
-# rouge_scores = evaluator.calc_score("rouge", ref, hypothesis)
-# print(f"Rouge Scores: {rouge_scores}")
-# blue_sfores =  evaluator.calc_score("blue", ref, hypothesis)
-# print(f"blue_sfores: {blue_sfores}")
-
-# from bleu import list_bleu
-# ref = ['it is a white cat .',
-#              'wow , this dog is huge .']
-# hyp = ['it is a white kitten .',
-#             'wowww , the dog is huge !']
-
-# ref = ['it is a white cat .', "it is a black elaphant"]
-# hyp = ['it is a white kitten .']
-# blue_scores =  evaluator.calc_score("blue", ref, hyp*2)
-# print(blue_scores)
-
-# print(multi_list_bleu([ref, ref1], [hyp, hyp1], detok=False))
-# print("done")
+ref = ['The quick brown fox jumps over the lazy dog', "Reference sentence"]
+hypothesis = [ 'The quick brown dog jumps on the log.', "Totally unrelated hypothesis"]
+rouge_scores = evaluator.calc_score("rouge", ref, hypothesis)
+print(f"Rouge Scores: {rouge_scores}")
+blue_scores =  evaluator.calc_score("blue", ref, hypothesis)
+print(f"blue_sfores: {blue_scores}")
